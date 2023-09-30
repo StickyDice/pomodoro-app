2985feef80d0b123a4340023ca7c151b1d14115f
Igor Lekomtsev <leckomtsevigor@yandex.ru>
2023-09-30 22:51:13 +0300

    tested recharts

--- src/components/Stats/Table/Chart/Chart.tsx
+++ src/components/Stats/Table/Chart/Chart.tsx
@@ -1,8 +1,57 @@
 import React from "react";
-import styles from "./chart.module.sass";
+import styles from "./chart.modules.sass";
+import { Bar, BarChart, CartesianGrid, ResponsiveContainer, XAxis, YAxis } from "recharts";
+import { scaleLog } from "d3-scale";
+
+const data = [
+   {
+      "day": "Пн",
+      value: 1,
+   },
+   {
+      "day": "Вт",
+      value: 0,
+   },
+   {
+      "day": "Ср",
+      value: 0,
+   },
+   {
+      "day": "Чт",
+      value: 0,
+   },
+   {
+      "day": "Пт",
+      value: 0,
+   },
+   {
+      "day": "Сб",
+      value: 0,
+   },
+   {
+      "day": "Вс",
+      value: 0,
+   },
+];
 
 export function Chart() {
    return (
-      <></>
+      <ResponsiveContainer width="100%" height="100%">
+         <BarChart data={data}>
+            <CartesianGrid vertical={false}/>
+            <XAxis
+               dataKey="day" axisLine={false} tickLine={false}
+            />
+            <YAxis
+               orientation="right"
+               scale="time"
+               // domain={[ 0, 2 ]}
+               // tickCount={5}
+               axisLine={false}
+               tickLine={false}
+            />
+            <Bar dataKey="value" fill="#EA8A79"/>
+         </BarChart>
+      </ResponsiveContainer>
    );
 }
--- src/components/Stats/Table/Table.tsx
+++ src/components/Stats/Table/Table.tsx
@@ -1,33 +1,39 @@
 import React from "react";
 import styles from "./table.module.sass";
 import tomato from "../../../images/emptyStatsTomato.svg";
+import classNames from "classnames";
+import { Chart } from "./Chart";
 
 export function Table() {
+   const bottomTitle = classNames(styles.boxTitle, styles.bottomTitle);
    return (
       <div className={styles.gridContainer}>
          <div className={styles.topContainer}>
             <div className={styles.topLeftContainer}>
                <div className={styles.box1}>
                   <h2 className={styles.boxTitle}>Суббота</h2>
-                  <p>Нет данных</p>
+                  <p className={styles.activity}>Нет данных</p>
                </div>
                <div className={styles.box3}>
                   <img className={styles.emptyTomato} src={tomato} alt="Помидорка"/>
                </div>
             </div>
-            <div className={styles.box2}>stats</div>
+            <div className={styles.box2}><Chart/></div>
          </div>
-         <div className={styles.box4}>
-            <h2 className={styles.boxTitle}>Фокус</h2>
-            <p>0%</p>
-         </div>
-         <div className={styles.box5}>
-            <h2 className={styles.boxTitle}>Время на паузе</h2>
-            <p>0м</p>
-         </div>
-         <div className={styles.box6}>
-            <h2 className={styles.boxTitle}>Остановки</h2>
-            <p>0</p>
+         <div className={styles.bottomContainer}>
+
+            <div className={styles.box4}>
+               <h2 className={bottomTitle}>Фокус</h2>
+               <p className={styles.bottom}>0%</p>
+            </div>
+            <div className={styles.box5}>
+               <h2 className={bottomTitle}>Время на паузе</h2>
+               <p className={styles.bottom}>0м</p>
+            </div>
+            <div className={styles.box6}>
+               <h2 className={bottomTitle}>Остановки</h2>
+               <p className={styles.bottom}>0</p>
+            </div>
          </div>
       </div>
    );
